ce1300cfd7ce349acbdbd07abd64dd09652a3a05
Ben Walding <bwalding@rambunctious.local>
2009-04-07 10:07:58 +1000

    Start using HAML / SASS

--- vendor/plugins/haml/init.rb
+++ vendor/plugins/haml/init.rb
@@ -1,12 +1,3 @@
-################################################################################
-#  Copyright (c) 2004-2009, by OpenXource, LLC. All rights reserved.
-#
-#  THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF OPENXOURCE
-#
-#  The copyright notice above does not evidence any
-#  actual or intended publication of such source code.
-################################################################################
-require 'rubygems'
 begin
   require File.join(File.dirname(__FILE__), 'lib', 'haml') # From here
 rescue LoadError
